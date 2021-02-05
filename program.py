9ea7ddaf17adae0b6ae2629b7878f1163f70824b
Levon Ritter <levong2112@gmail.com>
2021-02-05 12:00:43 -0500

    finished writing hash function and getting the script to read the file line by line

--- program.py
+++ program.py
@@ -1,1 +1,20 @@
-print("hello world")
+import hash_function
+import sys
+
+# test = hash_function.hash("hello world")
+
+# print(test)
+
+arr_of_lines = []
+
+file_to_hash = open("file_to_hash.txt", "r")
+for line in file_to_hash.readlines(): #file.readlines(), splits the file into a list, where each element is a seperate line
+  arr_of_lines.append(line)
+#  print(len(arr_of_lines))
+# print ("Name of the file: ", file_to_hash.name)
+# line = file_to_hash.readline()
+# print ("Read Line: %s" % (line))
+# Close opened file
+
+
+file_to_hash.close()
