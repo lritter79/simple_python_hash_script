b596461d19571eac8180a4cc96af61b7e69da0b9
Levon Ritter <levong2112@gmail.com>
2021-02-05 12:22:39 -0500

    finished it, hashes strings like a dream

--- program.py
+++ program.py
@@ -9,12 +9,20 @@ arr_of_lines = []
 
 file_to_hash = open("file_to_hash.txt", "r")
 for line in file_to_hash.readlines(): #file.readlines(), splits the file into a list, where each element is a seperate line
-  arr_of_lines.append(line)
+   temp_arr = line.split('=')
+   temp_str = temp_arr[0] + " = " + hash_function.hash(temp_arr[1]).decode("utf-8") 
+   print(temp_str)
+   arr_of_lines.append(temp_str)
 #  print(len(arr_of_lines))
 # print ("Name of the file: ", file_to_hash.name)
 # line = file_to_hash.readline()
 # print ("Read Line: %s" % (line))
 # Close opened file
 
-
 file_to_hash.close()
+
+with open("result.txt", 'w') as writer:
+    for line in arr_of_lines:
+        writer.write(line + "\n")
+
+writer.close
